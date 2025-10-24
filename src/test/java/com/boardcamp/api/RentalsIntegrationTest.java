6babab7e9bb672dafcc2220eb4b383f7be57e594
Jorge Koch <jorgeluizkoch@gmail.com>
2025-10-24 15:57:32 -0300

    Add integration tests for Rentals; implement rental creation and update tests

--- src/test/java/com/boardcamp/api/RentalsIntegrationTest.java
+++ src/test/java/com/boardcamp/api/RentalsIntegrationTest.java
@@ -8,13 +8,16 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.boardcamp.api.dtos.RentalsDTO;
 import com.boardcamp.api.models.CustomersModel;
 import com.boardcamp.api.models.GamesModel;
 import com.boardcamp.api.models.RentalsModel;
@@ -102,16 +105,135 @@ public class RentalsIntegrationTest {
     @Test
     void givenNonExistingRentalId_whenGettingRentalById_thenReturnEmpty() {
         // when
-        ResponseEntity<RentalsModel> response = testRestTemplate.exchange(
+        ResponseEntity<String> response = testRestTemplate.exchange(
             "/rentals/1", 
             HttpMethod.GET, 
             null, 
-            RentalsModel.class
+            String.class
             );  
         // then
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-        assertEquals(null, response.getBody());
+        assertEquals("Rental ID does not exist.", response.getBody());
+    }
+
+    @Test
+    void givenExistingRentalId_whenGettingRentalById_thenReturnRental() {
+         // given
+        CustomersModel customer = customersRepository.save(new CustomersModel(
+            null, 
+            "Test", 
+            "12345678900", 
+            "12345678910")
+            );
+        GamesModel game = gamesRepository.save(new GamesModel(
+            null, 
+            "test", 
+            "url", 
+            5, 
+            10)
+            );
+        RentalsModel rental = rentalsRepository.save(new RentalsModel(
+            null, 
+            LocalDate.now(), 
+            3, 
+            null, 
+            30, 
+            0, 
+            customer, 
+            game)
+            );
+
+        // when
+        ResponseEntity<String> response = testRestTemplate.exchange(
+            "/rentals/" + rental.getId(), 
+            HttpMethod.GET, 
+            null, 
+            String.class
+            );
+
+        // then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, List.of(response.getBody()).size());
+    }
+
+    @Test
+	void givenValidIds_whenCreatingRental_thenCreatesRental() {
+                // given
+        CustomersModel customer = customersRepository.save(new CustomersModel(
+            null, 
+            "Test", 
+            "12345678900", 
+            "12345678910")
+            );
+        GamesModel game = gamesRepository.save(new GamesModel(
+            null, 
+            "test", 
+            "url", 
+            5, 
+            10)
+            );
+
+        RentalsDTO rental = new RentalsDTO(
+            customer.getId(), 
+            game.getId(), 
+            3
+            );
+
+        HttpEntity<RentalsDTO> request = new HttpEntity<>(rental);
+
+        // when
+        ResponseEntity<String> response = testRestTemplate.exchange(
+            "/rentals", 
+            HttpMethod.POST, 
+            request, 
+            String.class
+            );
+
+        // then
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
     }
 
 
-}
+    @Test
+    void givenValidRentalId_whenUpdatingRental_thenUpdatesRental() {
+        // given
+        CustomersModel customer = customersRepository.save(new CustomersModel(
+            null, 
+            "Test", 
+            "12345678900", 
+            "12345678910")
+            );
+        GamesModel game = gamesRepository.save(new GamesModel(
+            null, 
+            "test", 
+            "url", 
+            5, 
+            10)
+            );
+        RentalsModel rental = rentalsRepository.save(new RentalsModel(
+            null, 
+            LocalDate.now().minusDays(5), 
+            3, 
+            null, 
+            30, 
+            0, 
+            customer, 
+            game)
+            );
+
+        // when
+        ResponseEntity<String> response = testRestTemplate.exchange(
+            "/rentals/" + rental.getId() + "/return", 
+            HttpMethod.POST, 
+            null, 
+            String.class
+            );
+
+        // then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        RentalsModel updatedRental = rentalsRepository.findById(rental.getId()).get();
+        assertEquals(LocalDate.now(), updatedRental.getReturnDate());
+        assertEquals(20, updatedRental.getDelayFee());
+    }
+
+}   
